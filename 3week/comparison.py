b21bb6ce85b72d0643b54d9e99ac158ae2311cb5
gunrein <gunrein620@gmail.com>
2025-09-18 14:13:34 +0900

    비교연산 예시 추가

---
+++ 3week/comparison.py
@@ -0,0 +1,8 @@
+a, b, c = 10, 20, 20
+
+print(f'{a}=={b}: {a==b}')
+print(f'{a}!={b}: {a!=b}')
+print(f'{a}>{b}: {a>b}')
+print(f'{b}<{c}: {b<c}')
+print(f'{a}>={b}: {a>=b}')
+print(f'{b}<={c}: {c<=c}')
