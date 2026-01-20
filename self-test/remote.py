31334807b62fd92a46632b386028d15edc8f5077
gunrein <gunrein620@gmail.com>
2026-01-21 00:22:04 +0900

    jupyter-setup

---
+++ self-test/remote.py
@@ -0,0 +1,26 @@
+a = 10;
+b = 20;
+int population = 0;
+
+print('안녕하세요 놀이공원 입니다.')
+
+while(true){ㅇㄹㅇㄹㄴㅇㄹsdfdsf
+  몇ㅇ
+}
+
+
+age = int(input("나이를 입력하세요: "))
+
+
+
+
+
+
+
+
+def is_audlt(){
+if age > 20:
+  print('성인 요금은 8,000원 입니다.')
+else:
+  print('미성년자는 5,000원 입니다.')
+}
